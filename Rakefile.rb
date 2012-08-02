1b17fdbeb5a043f159dc35ff74780bb5cb7d8754
Victor Deryagin <vderyagin@gmail.com>
2012-08-02 08:51:16 +0300

    rake task for generating README.md (automatically includes output of "nrename --help")

--- Rakefile.rb
+++ Rakefile.rb
@@ -9,3 +9,14 @@ RSpec::Core::RakeTask.new
 Cucumber::Rake::Task.new
 
 task :default => [:spec, :cucumber]
+
+desc 'Generate README.md'
+file 'README.md' => 'README.md.erb' do
+  require 'erb'
+
+  result = ERB.new(File.read './README.md.erb').result
+
+  File.open './README.md', 'w' do |file|
+    file.puts result
+  end
+end
