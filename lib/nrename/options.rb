5ffc2f8a6d48830832a34a0f7106c4132504ae7a
Victor Deryagin <vderyagin@gmail.com>
2012-09-07 14:01:43 +0300

    another workaround, this time for mri-1.8

--- lib/nrename/options.rb
+++ lib/nrename/options.rb
@@ -120,8 +120,11 @@ module Nrename
       if options.recursive
         dirs.dup.each do |dir|
           # Workaround: every other engine except jruby does not
-          # work with unescaped path:
-          dir = dir.shellescape unless RUBY_ENGINE == 'jruby'
+          # work with unescaped path, also mri-1.8 does not have
+          # constant RUBY_ENGINE defined.
+          unless defined? RUBY_ENGINE and RUBY_ENGINE == 'jruby'
+            dir = dir.shellescape
+          end
 
           Dir.glob(File.join dir, '**/') do |subdir|
             dirs << subdir.chomp('/')
