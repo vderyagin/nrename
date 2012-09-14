8b3b39c032ef755f976a0ac8a2644a78e0fa50ae
Victor Deryagin <vderyagin@gmail.com>
2012-09-14 19:28:29 +0300

    Utils: use Array instead of Set

--- lib/nrename/utils.rb
+++ lib/nrename/utils.rb
@@ -1,19 +1,18 @@
 require 'pathname'
-require 'set'
 
 module Nrename
   module Utils
     def all_subdirs_of(dir)
       dir = Pathname.new dir
 
-      children = Set.new
+      children = []
 
       dir.children.select(&:directory?).map(&:to_s).each do |child|
-        children.add child
-        children.merge all_subdirs_of child
+        children << child
+        children.concat all_subdirs_of child
       end
 
-      Array children
+      children
     end
     module_function :all_subdirs_of
   end
